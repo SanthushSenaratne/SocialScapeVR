e0a554283ec8c038258332272c90fdf0483b9d17
ChamikaCSA <127096458+ChamikaCSA@users.noreply.github.com>
2024-03-30 18:44:52 +0530

    Menu Completion

--- SocialScapeVR/Assets/MainMenu/MainMenu.cs
+++ SocialScapeVR/Assets/MainMenu/MainMenu.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject StartMenu;
+    public GameObject OptionsMenu;
+    public Button buttonToSelectInStartMenu;
+    public Button buttonToSelectInOptionsMenu;
 
+    void Start()
+    {
+        buttonToSelectInStartMenu.Select();
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
-       
+
+    public void LoadOptions()
+    {
+        StartMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+        buttonToSelectInOptionsMenu.Select();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }  
+
+    public void Back()
+    {
+        StartMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+        buttonToSelectInStartMenu.Select();
+    }
 
     public void QuitGame()
     {
